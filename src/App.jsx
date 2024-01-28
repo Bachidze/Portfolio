ac976d2b5b845c9754681c246ee7ccfe56a5bc02
bachidze <bachidze90@gmail.com>
2024-01-29 01:04:02 +0400

    feat: add Cursor follower

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,47 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './Components/Header/Header'
-import Main from './Components/Main/Main'
-import Projects from './Components/Projects/Projects'
-import Footer from './Components/Footer/Footer'
+import { useState } from 'react';
+import './App.css';
+import Header from './Components/Header/Header';
+import Main from './Components/Main/Main';
+import Projects from './Components/Projects/Projects';
+import Footer from './Components/Footer/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
   return (
     <>
-    <Header/>
-    <Main/>
-    <Projects/>
-    <Footer/>
+      <div
+        onPointerMove={(e) => {
+          setPosition({ x: e.clientX, y: e.clientY });
+        }}
+        className='gela'
+        style={{ position: 'relative' }} 
+      >
+        <Header />
+        <Main />
+        <Projects />
+        <Footer />
+        <div
+          style={{
+            position: 'fixed',
+            width: 50,
+            height: 50,
+            borderRadius: '50%',
+            background: 'transparent', 
+            border: '2px solid white',
+            transform: 'translate(-50%, -50%)',
+            left: `${position.x}px`,
+            top: `${position.y}px`,
+            pointerEvents: 'none', 
+            zIndex: 9999, 
+          }}
+        ></div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
+
